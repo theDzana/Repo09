7f5491a5f15ea8279bbd08824af89b1f3d9afbbf
Dedo-unuka <dzanazovko@gmail.com>
2022-04-05 12:54:21 +0200

    Adding ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -4,13 +4,13 @@ function comparisonToEqual(a) {
         return "Less than 5";
     }
     if( a < 10) {
-        return "Less than 10"
+        return "Less than 10";
     }
     if(a < 20) {
-        return "10 or over"
+        return "10 or over";
     }
     if(a > 20) {
-        return "More than 20"
+        return "More than 20";
     }
 }
 comparisonToEqual(0);
