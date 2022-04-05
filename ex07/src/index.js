179a500a49d065d5cbd5744c0f797b95165ae054
Dedo-unuka <dzanazovko@gmail.com>
2022-04-05 21:41:03 +0200

    Adding ex07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -2,7 +2,7 @@
 function randomRangeNumber(minNumber, maxNumber) {
     return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
 }
-randomRangeNumber(22, 7);
-console.log(randomRangeNumber(22, 7));
+randomRangeNumber(7, 22);
+console.log(randomRangeNumber(7, 22));
 // Only change code above this line
 module.exports = randomRangeNumber;
