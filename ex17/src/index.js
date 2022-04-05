55d5891ad458fb5779dd8801f0d5e735fb8ea0dd
Dedo-unuka <dzanazovko@gmail.com>
2022-04-05 22:53:23 +0200

    Adding ex17

---
+++ ex17/src/index.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+function inverseWhile() {
+    var fiveNumbers = "";
+    var  i = 5;
+    while (i >= 0) {
+        if (i === 0) {
+        fiveNumbers = fiveNumbers + i;
+    } else {
+        fiveNumbers = fiveNumbers + i + ",";
+    }
+         i--;
+        
+    }
+    return fiveNumbers;
+}
+inverseWhile();
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
